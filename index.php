a82967597e1f4faf5663330587f3c7e9a8007ddf
Greg0312 <greg@penguinmarketing.com>
2015-12-15 15:41:04 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,307 @@
+<?php
+/*
+ini_set('session.cache_limiter', 'private');
+header("Cache-Control: no-cache, must-revalidate,post-check=0,pre-check=0");
+include('../config/constants.php');
+$_SESSION['ipstate'] = getip_proipapi();
+recordreferer(); 
+$funnel_link = BASE_PATH_USA."pages/security-notice.php";
+$_SESSION['countrycode'] = 'USA';
+$doctoronline=rand(8,8);
+*/
+$blinkspeed=1000;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Split Test Contest JMA</title>
+
+<!-- Bootstrap -->
+<!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+<link href="css/bootstrap-3.3.4.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/font-awesome.css">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+	.blink_txt{ color:#fff; font-size:13px; font-weight:600; float: right; width:180px}
+.blink_txt .faa{ color:#0edf2b; font-size:14px; width:10px; height:10px; border-radius:10px; background:#0edf2b; display:inline-block;}
+#tits h1{ margin:0; padding:0; font-size:50px; font-weight:700; text-shadow:1px 1px 2px #000000; line-height:35px; margin-bottom:10px;}
+#tits h2{ margin:0; padding:0; font-size:25px; font-weight:400; text-shadow:1px 1px 2px #000000}
+#tits h3{ margin:0; padding:0; font-size:17px; font-weight:600; text-shadow:1px 1px 2px #000000; margin-top:20px;}
+#tits h3 span{font-size:20px; font-weight:600; text-shadow:1px 1px 2px #000000; margin-top:20px;}
+#tits1 {border: 2px solid #a1a1a1; padding: 10px; background: #dddddd;border-radius: 25px;}
+#tits li{color: #CCCB0E; font-weight:bold; background-color:#BDA0CB;}
+
+select {
+	/*
+    padding:3px;
+    margin: 0;
+    -webkit-border-radius:4px;
+    -moz-border-radius:4px;
+    border-radius:4px;
+    -webkit-box-shadow: 0 3px 0 #ccc, 0 -1px #fff inset;
+    -moz-box-shadow: 0 3px 0 #ccc, 0 -1px #fff inset;
+    box-shadow: 0 3px 0 #ccc, 0 -1px #fff inset;
+    background: #f8f8f8;
+    color:#888;
+    border:none;
+    outline:none;
+    display: inline-block;
+    -webkit-appearance:none;
+    -moz-appearance:none;
+    appearance:none;
+    cursor:pointer;
+	*/
+	
+	font-size:18px; font-weight:bold; text-shadow:#A6A6A6 1px 1px 3px; background-color:#F8E004;
+}
+#matter{max-width:780px; max-height:350px; width:100%; height:150px; margin-right:10px; margin-left:0px;}
+	</style>
+</head>
+<body>
+<header>
+<div class="container">
+<div class="row" id="tits">
+<p>
+<div class="col-xs-12 col-sm-6 col-lg-8" >
+
+<h2>JAMES MORGAN.Attorney Nevada</h2>
+
+
+</div>
+
+<div class="col-xs-6 col-lg-4 hidden-xs">
+<a href="https://member.jamesmorgan.attorney/index.php"><h2 align="right">Login</h2></a>
+</div>
+</p>
+  </div>
+  <hr>
+</div>
+</header>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12" >
+      <div class="jumbotron"  id="tits" >
+        <h2 class="text-center">Ask a Lawyer Online any Question</h2>
+        <p>&nbsp;</p>
+        <div class="container" style="background-color:#452f6b; max-width:810px; width:100%; border-radius:15px; padding:25px">
+         
+        <form action="https://www.jamesmorgan.attorney/pages/age.php" method="post" name="frm1" id="frm1">
+
+<div class="form-group">
+      <p></p>
+      <div class="clearfix"></div>
+     <!--<div class="col-xs-12 col-sm-6 col-md-8" style="margin-left:-10px;">-->
+        <select class="form-control" style="color:#452f6b">
+          <option>Select Category</option>
+  <option value="1">Family Law</option>
+  <option value="2">Criminal Law</option>
+  <option value="3">Business Law</option>
+  <option value="4">Consumer Law</option>
+  <option value="5">Real Estate Law</option>
+  <option value="6">Immigration Law</option>
+  <option value="7">Bankruptcy Law</option>
+    <option value="8">Traffic Law</option>
+      <option value="9">Other</option>
+        </select> 
+     <!-- </div>-->
+    </div>
+    
+ <!--
+<div class="col-lg-12 col-md-12 col-sm-12">
+<ul class="nav nav-stacked">
+  <li ><a  href="#" id="hrmenu0" >General Law</a></li>
+  <li>
+   
+   
+ 
+ 
+  
+   
+    <a  class="dropdown-toggle" data-toggle="dropdown" href="#">Select Category
+    <span class="caret"></span></a>
+    <ul class="dropdown-menu">
+    <li class="caption"><a  href="#" id="hrmenu1" >Family Law</a></li>
+    <li><a  href="#"  id="hrmenu2">Criminal Law</a></li>
+    <li><a  href="#"  id="hrmenu3" >Business Law</a></li>
+    <li><a  href="#"  id="hrmenu4" >Consumer Law</a></li>
+    <li><a  href="#" id="hrmenu9" >Real Estate Law</a></li>
+    <li><a  href="#" id="hrmenu5" >Immigration Law</a></li>
+    <li><a  href="#" id="hrmenu6" >Bankruptcy Law</a></li>
+    <li><a  href="#" id="hrmenu7" >Traffic Law</a></li>
+    <li><a  href="#" id="hrmenu8" >Other</a></li>
+    </ul>
+   
+  </li>
+ 
+</ul> -->
+ <h2 style="color:#FFFF0F; padding-top:20px">Please explain your legal question below</h2><br>
+<textarea name="matter" id="matter" required placeholder="Hi, can I answer your legal question? Please type your question here..." ></textarea><br><br>
+<div class="blink_txt doc-online"><span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span> <span id="blnktextonline" style="display: none;color:#FFFF0F">8 Lawyers online</span></div>
+<button class="btn btn-warning"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Ask Lawyer Online</button>
+<p>&nbsp;</p>
+</div>
+</div>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <!--<h1>Pricing Table</h1>-->
+    <hr>
+    <div class="col-xs-6">
+      <div class="panel panel-default panel-success"> 
+        <!-- Default panel contents -->
+        <div class="panel-heading">
+          <h3>Recently Asked Questions:</h3>
+        </div>
+        <!-- Table -->
+        <table class="table">
+          
+          <tbody>
+            <tr>
+              <th scope="row">What is the statute of limitation for a dad to come into the picture?</th>
+            </tr>
+            <tr>
+              <th scope="row">Can I sue a news station for running an investigative story?</th>
+            </tr>
+            <tr>
+              <th scope="row">Do I have legal rights to pick up my step daughter from her mother?</th>
+            </tr>
+            <tr>
+              <th scope="row"><p class="text-center"><button class="btn-success btn"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Ask Lawyer</button></p></th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+     
+</div>
+    
+    <div class="col-xs-6">
+      <div class="panel panel-default panel-info"> 
+        <!-- Default panel contents -->
+        <div class="panel-heading">
+          <h3>Legal Questions recently answered</h3>
+        </div>
+        <!-- Table -->
+        <table class="table">
+        
+          <tbody>
+            <tr>
+              <th scope="row">Do I have legal rights to pick up my step daughter from her mother?</th>
+            </tr>
+            <tr>
+              <th scope="row">How can I legally get my 25 year old son out of the house in WA State?</th>
+            </tr>
+            <tr>
+              <th scope="row">What is the statute of limitation for a dad to come into the picture?</th>
+            </tr>
+            <tr>
+              <th scope="row"><p class="text-center"><button href="" class="btn-warning btn"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Ask Lawyer</button></p>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    </form>
+   
+  </div>
+</div>
+<hr>
+<div class="container">
+ 
+</div>
+  <hr>
+<footer class="text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+      
+ <p>LIVE CUSTOMER SERVICE |  ABOUT |  WORK WITH US |  TERMS OF SERVICE |  PRIVACY |  CONTACT |  CLOSE MY ACCOUNT |  SITEMAP |  BLOG |  LOGIN  |  LAWYERS APPLY HERE </p>
+        <p>Copyright ©  JamesMorgan.attorney. All rights reserved.</p>
+        <p></p>
+      </div>
+    </div>
+  </div>
+</footer>
+<script>
+
+var myString = "Hi, can I answer your legal question? Please type your question here...";
+var myArray = myString.split("");
+var loopTimer;
+function frameLooper() 
+{
+ if(document.getElementById('stopautotyping').value=='no')
+ {
+	if(myArray.length > 0) {
+		document.getElementById("matter").value += myArray.shift();
+	} else {
+		clearTimeout(loopTimer); 
+                return false;
+	}
+	loopTimer = setTimeout('frameLooper()',70);
+ }
+}
+function frameLooper_blank()
+{
+document.getElementById("matter").value="";
+}
+function blinktextdiv(dividtoblink)
+{
+  if(document.getElementById(dividtoblink).style.display=='none')
+  {
+   document.getElementById(dividtoblink).style.display="";
+  }
+  else
+  {
+   document.getElementById(dividtoblink).style.display="none";
+  }
+  setTimeout("blinktextdiv('"+dividtoblink+"')",<?php echo $blinkspeed?>);
+}
+
+function addremoveclass(ctab)
+	{
+	  for(var ti=0;ti<=9;ti++)
+	  {
+	   //document.getElementById("hrmenu"+ti).className = "";
+	   if ( $('#hrmenu'+ti).hasClass('active') )
+	   {
+	   $('#hrmenu'+ti).removeClass('active');
+	   }
+	  }
+	  $('#hrmenu'+ctab).addClass('active');
+	}
+	
+function stoptyping()
+{
+document.getElementById('stopautotyping').value='yes';
+ if(document.getElementById('firstclick').value=='no')
+ {
+ document.getElementById('matter').value="";
+  document.getElementById('firstclick').value="yes";
+ }
+}
+
+setTimeout("frameLooper()",3000);
+setTimeout("frameLooper_blank()",2500);
+setTimeout("blinktextdiv('blnktextonline')",500);
+</script>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<!-- <script src="js/bootstrap.min.js"></script> -->
+<script src="js/bootstrap-3.3.4.js" type="text/javascript"></script>
+</body>
+</html>
